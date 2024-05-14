bd521923289b062c2bfac4a28929276169b3d760
GlezovDmitriy <dmitriy_glezov@mail.ru>
2024-05-15 00:07:40 +0300

    added GET-request

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -3,8 +3,11 @@ import {useEffect} from "react";
 import {decksApi} from "../decks-api.ts";
 
 export const DecksList = () => {
+
   useEffect(() => {
     decksApi.getDecks()
   }, []);
+
   return <ul className={s.list}></ul>
+
 }
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -1,22 +1,19 @@
 import axios from 'axios'
 
 type ItemsType = {
-    [
-        {
-            "author": {
-                "id": "string",
-                "name": "string"
-            },
-            "id": "string",
-            "userId": "string",
-            "name": "string",
-            "isPrivate": true,
-            "cover": "string",
-            "created": "2024-05-14T20:35:02.252Z",
-            "updated": "2024-05-14T20:35:02.252Z",
-            "cardsCount": 0
-        }
-        ]
+    author: AuthorType,
+    id: string,
+    userId: string,
+    name: string,
+    isPrivate: boolean,
+    cover: string,
+    created: string,
+    updated: string,
+    cardsCount: number
+}
+type AuthorType = {
+    id: string,
+    name: string
 }
 type PaginationType = {
     currentPage: number,
@@ -26,7 +23,7 @@ type PaginationType = {
 
 }
 export type GetDecksType = {
-    items: ItemsType,
+    items: ItemsType[],
     pagination: PaginationType
 }
 export const instance = axios.create({
