eb8ceddd88570b73b9fc9357559c79d5fe68b7b2
GlezovDmitriy <dmitriy_glezov@mail.ru>
2024-05-16 01:27:58 +0300

    refactored DecksList

--- src/features/decks/Decks.tsx
+++ src/features/decks/Decks.tsx
@@ -1,5 +1,6 @@
 import { DecksList } from './DecksList/DecksList.tsx'
 import { AddNewDeckForm } from './AddNewDeckForm/AddNewDeckForm.tsx'
+import {DeckItem} from "./DecksList/DeckItem/DeckItem.tsx";
 
 export const Decks = () => {
   return (
--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,14 +1,25 @@
 import s from './DecksList.module.css'
-import {useEffect} from "react";
-import {decksApi} from "../decks-api.ts";
+import {useEffect, useState} from "react";
+import {decksApi, ItemsType} from "../decks-api.ts";
 import {setDecksAC} from "../decks-reducer.ts";
-
+import dispatch from "redux"
+import {useDispatch} from "react-redux";
+import {DeckItem} from "./DeckItem/DeckItem.tsx";
 export const DecksList = () => {
-
+  const [decks, setDecks] = useState<ItemsType[]>([])
+  const dispatch = useDispatch();
   useEffect(() => {
-    decksApi.getDecks()
+    decksApi.getDecks().then(res => dispatch(setDecksAC(res.data.items)) && setDecks(res.data.items))
+
   }, []);
 
-  return <ul className={s.list}></ul>
+
+  return <ul className={s.list}>
+    {decks.map(el =>(
+        <DeckItem key={el.id} deck={el} />
+        )
+    )}
+
+  </ul>
 
 }
